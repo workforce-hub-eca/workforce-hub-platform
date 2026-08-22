1a1af942c56683380ced8d58af19e811db614c02
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-08-22 23:05:02 +0530

    chore(deploy): add PM2 config for Config Server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,22 @@ module.exports = {
       restart_delay: 5000,
       out_file: '/var/log/workforce-hub/eureka-out.log',
       error_file: '/var/log/workforce-hub/eureka-error.log'
+    },
+    {
+      name: 'config-server',
+      script: '/usr/bin/java',
+      args: '-jar config-server-0.0.1-SNAPSHOT.jar',
+      cwd: '/opt/workforce-hub/apps',
+      interpreter: 'none',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      restart_delay: 5000,
+      out_file: '/var/log/workforce-hub/config-out.log',
+      error_file: '/var/log/workforce-hub/config-error.log',
+      env: {
+        CONFIG_SERVER_EUREKA_URL: 'http://localhost:8761/eureka/'
+      }
     }
   ]
 };
