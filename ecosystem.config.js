0b73bb131da68090efc82b6a92510c27065ea7e4
L.K.H.M Lakdiw <manuthlakdiw2006@gmail.com>
2026-08-23 18:43:56 +0530

    feat: add API Gateway GCP deployment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -28,6 +28,24 @@ module.exports = {
       env: {
         CONFIG_SERVER_EUREKA_URL: 'http://localhost:8761/eureka/'
       }
+    },
+    {
+      name: 'api-gateway',
+      script: '/opt/workforce-hub/scripts/start-api-gateway.sh',
+      cwd: '/opt/workforce-hub/apps',
+      interpreter: 'none',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
+      restart_delay: 5000,
+      out_file: '/var/log/workforce-hub/api-gateway-out.log',
+      error_file: '/var/log/workforce-hub/api-gateway-error.log',
+      env: {
+        SPRING_PROFILES_ACTIVE: 'gcp',
+        CONFIG_SERVER_URL: 'http://localhost:8888',
+        API_GATEWAY_EUREKA_URL: 'http://localhost:8761/eureka/',
+        API_GATEWAY_CORS_ALLOWED_ORIGINS: 'http://localhost:5173,http://127.0.0.1:5173'
+      }
     }
   ]
 };
